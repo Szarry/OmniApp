b31868f7cd04d239b07e24a885e5fcee6e752552
Szarry <mikolaj.pytlarczyk@gmail.com>
2023-05-21 20:24:26 +0200

    Initial commit

---
+++ src/test/java/pl/szarry/omniapp/OmniAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szarry.omniapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmniAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
